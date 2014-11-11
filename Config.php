352e2520c7d1f3211c2f86b96a4324dea71bac6d
Nicolas Mahé <nicolas@mahe.me>
2014-11-11 10:33:20 +0100

    Update Config.php

--- Config.php
+++ Config.php
@@ -2,29 +2,29 @@
 
 class Config
 {
-	private static $config = null;
-	
-	private static function init()
-	{
-		self::$config = Storage::read('config.json');
-		
-		if(empty(self::$config)) {
-			Error::add('Config file empty');
-		}
-	}
-	
-	public static function get($key)
-	{
-		if(self::$config === null) {
-			self::init();
-		}
-		
-		if(isset(self::$config[$key])) {
-			return self::$config[$key];
-		}
-		
-		Error::add('Key "'.$key.'" is unknown');
-		
-		return;
-	}
+    private static $config = null;
+    
+    private static function init()
+    {
+        self::$config = Storage::read('config.json');
+        
+        if(empty(self::$config)) {
+            Error::add('Config file empty');
+        }
+    }
+    
+    public static function get($key)
+    {
+        if(self::$config === null) {
+            self::init();
+        }
+        
+        if(isset(self::$config[$key])) {
+            return self::$config[$key];
+        }
+        
+        Error::add('Key "'.$key.'" is unknown');
+        
+        return;
+    }
 }
