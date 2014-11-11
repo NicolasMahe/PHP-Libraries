fa2c9060441793d4c5e2771d924d6fc5d1821779
Nicolas Mahé <nicolas@mahe.me>
2014-11-11 10:27:07 +0100

    Update Log.php

--- Log.php
+++ Log.php
@@ -2,10 +2,10 @@
 
 class Log
 {
-	private static $data = array();
+    private static $data = array();
 
-	public static function add($method, $action, $status, $message)
-	{
+    public static function add($method, $action, $status, $message)
+    {
         if(!empty($message)) {
             self::$data[] = array(
                 'method' => ucfirst($method),
@@ -22,36 +22,36 @@ class Log
                 'date' => date("Y-m-d\TH:i:s\Z")
             );
         }
-	}
-	
-	public static function write()
-	{
-		if(!empty(self::$data)) {
-			$oldLog = self::getAll();
-			if(!empty($oldLog))
-				self::$data = array_merge($oldLog, self::$data);
-				
-			$retour = Storage::write(Config::get('storagePathLog'), self::$data);
-			
-			if($retour) {
-				return true;
-			} else {
-				Error::add('error during writing the log to the storage file');
-				return false;
-			}
-		}
-	}
-	
-	public static function getAll()
-	{
-		$log = Storage::read(Config::get('storagePathLog'));
-		
-		if(!empty($log)) {
-			return $log;
-		} else {
-			Error::add('error during reading the log file');
-		}
-	}
+    }
+    
+    public static function write()
+    {
+        if(!empty(self::$data)) {
+            $oldLog = self::getAll();
+            if(!empty($oldLog))
+                self::$data = array_merge($oldLog, self::$data);
+                
+            $retour = Storage::write(Config::get('storagePathLog'), self::$data);
+            
+            if($retour) {
+                return true;
+            } else {
+                Error::add('error during writing the log to the storage file');
+                return false;
+            }
+        }
+    }
+    
+    public static function getAll()
+    {
+        $log = Storage::read(Config::get('storagePathLog'));
+        
+        if(!empty($log)) {
+            return $log;
+        } else {
+            Error::add('error during reading the log file');
+        }
+    }
     
     public static function reset()
     {
