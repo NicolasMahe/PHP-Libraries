9fed6f73b3ec93a3d4be58890a5d5dd26cadf520
Nicolas Mahé <nicolas@mahe.me>
2014-11-11 10:28:20 +0100

    Update Response.php

--- Response.php
+++ Response.php
@@ -2,27 +2,27 @@
 
 class Response
 {
-	private static $data;
-	private static $status;
-	
-	public static function setData($data)
-	{
-		self::$data = $data;
-	}
+    private static $data;
+    private static $status;
+    
+    public static function setData($data)
+    {
+        self::$data = $data;
+    }
 
-	public static function setStatus($status)
-	{
-		self::$status = $status;
-	}
-	
-	public static function send()
-	{
-		$response = array(
-			'status' => self::$status,
-			'data' => self::$data,
-			'error' => Error::getAll()
-		);
-		
-		echo json_encode($response);
-	}
+    public static function setStatus($status)
+    {
+        self::$status = $status;
+    }
+    
+    public static function send()
+    {
+        $response = array(
+            'status' => self::$status,
+            'data' => self::$data,
+            'error' => Error::getAll()
+        );
+        
+        echo json_encode($response);
+    }
 }
