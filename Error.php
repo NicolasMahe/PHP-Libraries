30adffc8ec2df7a3621c3a6ddcc5680e479d8e1e
Nicolas Mahé <nicolas@mahe.me>
2014-11-11 10:11:17 +0100

    Create Error.php

---
+++ Error.php
@@ -0,0 +1,16 @@
+<?php
+
+class Error
+{
+	private static $error = array();
+	
+	public static function add($error)
+	{
+		self::$error[] = $error;
+	}
+	
+	public static function getAll()
+	{
+		return self::$error;
+	}
+}
