a0a681cc9b679a1250fd0f3177cf9b604e3387d8
Nicolas Mahé <nicolas@mahe.me>
2014-11-11 10:27:44 +0100

    Update Request.php

--- Request.php
+++ Request.php
@@ -2,35 +2,29 @@
 
 class Request
 {
-	private static $post = null;
-	
-	private static function init()
-	{
-		self::$post = json_decode(file_get_contents("php://input"), true);
-	}
-	
-	public static function get($key)
-	{
-		if(isset($_GET[$key]))
-		{
-			return $_GET[$key];
-		}
-		
-		return;
-	}
-	
-	public static function post($key)
-	{
-		if(self::$post === null)
-		{
-			self::init();
-		}
-		
-		if(isset(self::$post[$key]))
-		{
-			return self::$post[$key];
-		}
-		
-		return;
-	}
+    private static $post = null;
+    
+    private static function init()
+    {
+        self::$post = json_decode(file_get_contents("php://input"), true);
+    }
+    
+    public static function get($key)
+    {
+        if(isset($_GET[$key])) {
+            return $_GET[$key];
+        }
+        return;
+    }
+    
+    public static function post($key)
+    {
+        if(self::$post === null) {
+            self::init();
+        }
+        if(isset(self::$post[$key])) {
+            return self::$post[$key];
+        }
+        return;
+    }
 }
