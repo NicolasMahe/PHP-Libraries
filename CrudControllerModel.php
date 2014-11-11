9856ef2c8c48f0415c149723a473ac8aed2560fa
Nicolas Mahé <nicolas@mahe.me>
2014-11-11 10:21:08 +0100

    Update CrudControllerModel.php

--- CrudControllerModel.php
+++ CrudControllerModel.php
@@ -8,15 +8,12 @@ abstract class CrudControllerModel
             
     function __construct($name, $verbose=false)
     {
-        if(!empty($name))
-        {
+        if(!empty($name)) {
             $this->storage = new Table($name);
             $this->verbose = $verbose;
             $this->name = $name;
-        }
-        else
-        {
-            Error::add('crud name is empty');
+        } else {
+            Error::add('name is empty');
             Response::setStatus('error');
         }
     }
@@ -25,12 +22,9 @@ abstract class CrudControllerModel
     {
         $id = Request::get('id');
         
-        if(!empty($id))
-        {
+        if(!empty($id)) {
             $data = $this->storage->get($id);
-        }
-        else
-        {
+        } else {
             $data = $this->storage->getAll();
         }
 		
@@ -42,30 +36,22 @@ abstract class CrudControllerModel
     {
         $data = Request::post('data');
 
-        if(!empty($data))
-        {
+        if(!empty($data)) {
             $retour = $this->storage->add($data);
             
-            if($retour)
-            {
-                if($this->verbose)
-                {
+            if($retour) {
+                if($this->verbose) {
                     Log::add($this->name, "add", "success", $this->name." item id '".($this->storage->getNextId() - 1)."' successfully added");
                 }
                 Response::setStatus('success');
-            }
-            else
-            {
-                if($this->verbose)
-                {
+            } else {
+                if($this->verbose) {
                     Log::add($this->name, "add", "error", $this->name." item id '".($this->storage->getNextId() - 1)."' error during add");
                 }
                 Error::add('error during add');
                 Response::setStatus('error');
             }
-        }
-        else
-        {
+        } else {
             Error::add('data is empty');
             Response::setStatus('error');
         }
@@ -75,30 +61,22 @@ abstract class CrudControllerModel
     {
         $data = Request::post('data');
 
-        if(!empty($data) && !empty($data['id']))
-        {
+        if(!empty($data) && !empty($data['id'])) {
             $retour = $this->storage->update($data);
             
-            if($retour)
-            {
-                if($this->verbose)
-                {
+            if($retour) {
+                if($this->verbose) {
                     Log::add($this->name, "update", "success", $this->name." item id '".$data['id']."' successfully updated");
                 }
                 Response::setStatus('success');
-            }
-            else
-            {
-                if($this->verbose)
-                {
+            } else {
+                if($this->verbose) {
                     Log::add($this->name, "update", "error", $this->name." item id '".$data['id']."' error during update");
                 }
                 Error::add('error during update');
                 Response::setStatus('error');
             }
-        }
-        else
-        {
+        } else {
             Error::add('data and/or id is empty');
             Response::setStatus('error');
         }
@@ -108,30 +86,22 @@ abstract class CrudControllerModel
     {
         $id = Request::get('id');
 
-        if(!empty($id))
-        {
+        if(!empty($id)) {
             $retour = $this->storage->delete($id);
             
-            if($retour)
-            {
-                if($this->verbose)
-                {
+            if($retour) {
+                if($this->verbose) {
                     Log::add($this->name, "delete", "success", $this->name." item id '".$id."' successfully deleted");
                 }
                 Response::setStatus('success');
-            }
-            else
-            {
-                if($this->verbose)
-                {
+            } else {
+                if($this->verbose) {
                     Log::add($this->name, "delete", "error", $this->name." item id '".$id."' error during delete");
                 }
                 Error::add('error during delete');
                 Response::setStatus('error');
             }
-        }
-        else
-        {
+        } else {
             Error::add('data and/or id is empty');
             Response::setStatus('error');
         }	
