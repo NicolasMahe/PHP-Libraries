54764ef0dc396d09c94453d96210943ee83ac3db
Nicolas Mahé <nicolas@mahe.me>
2014-11-11 10:12:52 +0100

    Create Response.php

---
+++ Response.php
@@ -0,0 +1,28 @@
+<?php
+
+class Response
+{
+	private static $data;
+	private static $status;
+	
+	public static function setData($data)
+	{
+		self::$data = $data;
+	}
+
+	public static function setStatus($status)
+	{
+		self::$status = $status;
+	}
+	
+	public static function send()
+	{
+		$response = array(
+			'status' => self::$status,
+			'data' => self::$data,
+			'error' => Error::getAll()
+		);
+		
+		echo json_encode($response);
+	}
+}
