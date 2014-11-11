cd0836a22e8d6688271a71aa8549266d316c8f1c
Nicolas Mahé <nicolas@mahe.me>
2014-11-11 10:13:45 +0100

    Create Table.php

---
+++ Table.php
@@ -0,0 +1,137 @@
+<?php
+
+class Table
+{
+    private $extension = '.json';
+    
+    private $name = null;
+    private $data = array();
+    private $nextId = 1;
+    private $hasBeenModify = false;
+    
+    function __construct($name)
+    {
+        if(!empty($name))
+        {
+            $filePath = Config::get('storagePath').$name.$this->extension;
+            $data = Storage::read($filePath);
+            
+            $this->name = $name;
+            
+            if($data)
+            {
+                if(!empty($data))
+                {
+                    if(!empty($data['data']) && !empty($data['nextId']))
+                    {
+                        $this->data = $data['data'];
+                        $this->nextId = $data['nextId'];
+                    }
+                }
+            }
+        }
+        else
+        {
+            Error::add('Table name is empty');
+        }
+    }
+
+    function __destruct()
+    {
+        if($this->hasBeenModify)
+        {
+            $arrayToWrite = array(
+                "nextId" => $this->nextId,
+                "data" => $this->data
+            );
+
+            $filePath = Config::get('storagePath').$this->name.$this->extension;
+            $retour = Storage::write($filePath, $arrayToWrite);
+
+            if(!$retour)
+            {
+                Error::add('Error during writing the storage for "'.$this->name.'"');
+            }
+        }
+    }
+    
+    public function add($data)
+    {
+        if(!empty($data) && is_array($data))
+        {
+            $data['id'] = $this->nextId;
+            $data['createdAt'] = date("Y-m-d H:i:s");
+            $this->data[] = $data;
+            $this->nextId++;
+            $this->hasBeenModify = true;
+            
+            return true;
+        }
+        else
+        {
+            Error::add('Data is empty or not an array');
+        }
+        
+        return false;
+    }
+    
+    public function get($id)
+    {
+        foreach ($this->data as $data)
+        {
+            if($data['id'] == $id)
+            {
+                return $data;
+            }
+        }
+        
+        return null;
+    }
+    
+    public function delete($id)
+    {
+        foreach ($this->data as $key => $data)
+        {
+            if($data['id'] == $id)
+            {
+                unset($this->data[$key]);
+                $this->hasBeenModify = true;
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public function update($dataUpdated)
+    {
+        foreach ($this->data as &$data)
+        {
+            if($data['id'] == $dataUpdated['id'])
+            {
+                $data = $dataUpdated;
+                $this->hasBeenModify = true;
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public function getAll()
+    {
+        return $this->data;
+    }
+    
+    public function getCount()
+    {
+        return count($this->data);
+    }
+    
+    public function getNextId()
+    {
+        return $this->nextId;
+    }
+}
